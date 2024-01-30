c06cdd4fcf55f4552ad76dc95d71322ffefa91f4
hyunchul jung <blackironj@gmail.com>
2024-01-30 12:30:22 +0900

    refactor: Imported libraries, created Git diff and commit message functions

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main () {
+import (
+	"fmt"
+	"os/exec"
 
+	"github.com/blackironj/easycommit/llm"
+)
+
+func main() {
+	diff, err := exec.Command("git", "diff", "--cached", "--diff-algorithm=minimal").Output()
+	if err != nil {
+		panic(err)
+	}
+
+	prompter := llm.NewPrompter("http://127.0.0.1:11434", llm.Mistral, 0.7, 100)
+	summary := prompter.GetSummary(string(diff), "")
+
+	commit := prompter.GetCommitMsg(summary, "")
+	fmt.Println("Generated commit msg : ", commit)
+
+	if err := exec.Command("git", "commit", "-m", commit).Run(); err != nil {
+		panic(err)
+	}
 }
